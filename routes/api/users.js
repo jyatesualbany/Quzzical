b1c7e82a8a29d426f38a2cc27d016e1da93bd2c2
Cory <cmarriott@albany.edu>
2019-04-04 19:00:31 -0400

    Revert "added functionality to the users api"
    This reverts commit 3ea9bea793d2ca8b0bfb6f82737ad465e7b28697.

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,59 +6,6 @@ const mysql = require('mysql');
 const connection = require('../../config/database.js');
 const validateLogin = require('../../validation/login');
 
-const db = connection.db
-
-router.post('/register', (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body)
-
-  if(!isValid){
-    return res.status(400).json(errors);
-  }
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  // query to find if email is in db
-  var selectEmail = "select count(*) as emailCount from USER where email = '"+email+"'"
-  db.query(selectEmail, (err, res) => {
-    if(err){
-      console.error('Error conecting: ' + err.stack);
-    }
-    if(res[0].emailCount != 0){
-     errors.email = 'Email already exists'
-     return res.status(400).json(errors);
-    }else{
-      // create new user
-      const newUser = ({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        isAdmin: req.body.admin
-      })
-
-      //insert new user to db
-      const insert = "insert into USER(NAME,IS_ADMIN, EMAIL, PASSWORD) VALUES ?"
-      bcrypt.getSalt(10, (err, salt) =>{
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if(err) throw err
-          newUser.password = hash
-          db.query(insert, newUser, (err, res) => {
-            if(err){
-              return console.error(err.stack);
-            }else{
-              console.log('hello new user');
-            }
-          })
-        })
-      })
-    }
-  })
-
-
-})
-
-
-
 //@route GET api/users/login
 //@desc Login user/ Returning JWT Token
 //@access Public
@@ -73,33 +20,23 @@ router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  var selectEmail = "select *from USER where email = '"+email+"'"
-
+  let db = connection.db
+  var selectEmail = "select exists(select * from USERS where email = '"+email+"')"
   db.query(selectEmail, function (err, res) {
     if(err){
       console.error('Error conecting: ' + err.stack);
     }
-    const pw = res[0].PASSWORD
-    bcrypt.compare(pw, user.password).then(isMatch => {
-      if(isMatch){
+    var row = JSON.stringify(res)
+    var arr = JSON.parse(row)
+    var pass = JSON.stringify(arr[0].PASSWORD).toString()
+    var len = pass.length
+    var p = pass.substring(1, len-1)
 
-        const payload = {email: user.email, isAdmin: user.isAdmin}
-        jwt.sign(
-          payload,
-          'secret',
-          {expiresIn: 3600},
-          (err, tok) => {
-            res.json({
-              success: true,
-              token: 'Bearer ' + tok
-            })
-          }
-        )
-      }else{
-        errors.password = 'Password incorrect'
-        return res.status(400).json(errors);
-      }
-    })
+    if(p == password){
+      jwt.sign(
+
+      )
+    }
 
   })
   //need to get info from database
