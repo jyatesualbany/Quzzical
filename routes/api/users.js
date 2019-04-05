ae61cf6a89d003eb953e078d2e51bd73d32363c9
Kostin <kgalusha95@gmail.com>
2019-04-04 21:14:02 -0400

    onSubmit works/ post does not

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,61 @@ const mysql = require('mysql');
 const connection = require('../../config/database.js');
 const validateLogin = require('../../validation/login');
 
+const validateRegister = require('../../validation/register');
+const db = connection.db
+
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegister(req.body)
+
+  if(!isValid){
+     return res.status(400).json(errors);
+  }
+
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // query to find if email is in db
+  var selectEmail = "select count(*) as emailCount from USER where email = '"+email+"'"
+  db.query(selectEmail, (err, res) => {
+    if(err){
+      console.error('Error connecting: ' + err.stack);
+    }
+    if(res[0].emailCount !== 0){
+     errors.email = 'Email already exists'
+     return res.status(400).json(errors);
+    }else{
+      // create new user
+      const newUser = ({
+        name: req.body.name,
+        isAdmin: req.body.isAdmin,
+        email: req.body.email,
+        password: req.body.password
+      })
+
+      //insert new user to db
+      const insert = "insert into USER(NAME,IS_ADMIN, EMAIL, PASSWORD) VALUES ?"
+      bcrypt.getSalt(10, (err, salt) =>{
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if(err) throw err
+          newUser.password = hash
+          db.query(insert, newUser, (err, res) => {
+            if(err){
+              return console.error(err.stack);
+            }else{
+              console.log('hello new user');
+            }
+          })
+        })
+      })
+    }
+  })
+
+
+})
+
+
+
 //@route GET api/users/login
 //@desc Login user/ Returning JWT Token
 //@access Public
