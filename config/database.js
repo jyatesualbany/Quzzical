32772ee80a43666d3f316d4537bafb0be6916944
Kostin <kgalusha95@gmail.com>
2019-04-08 13:58:52 -0400

    done

--- config/database.js
+++ config/database.js
@@ -1,30 +1,10 @@
-const express = require('express');
-const mysql = require('mysql');
-
-<<<<<<< Updated upstream
-=======
 
 
 
->>>>>>> Stashed changes
-
-const connection = mysql.createConnection({
-  host: 'quizzical.chkqldbkslgu.us-east-2.rds.amazonaws.com',
-  user: 'QUIZZICAL',
-  password: 'QUIZZICAL418y',
-  database: 'QUIZZICAL',
-  port: 3306,
-  acquireTimeout: 1000000
-});
-
-connection.connect(function (err) {
-  if(err) throw err;
-  console.log('Connected to the database');
-})
 
 
-module.exports = {
-  db : connection
-}
 
+
+module.exports ={
+    db: connection
 
