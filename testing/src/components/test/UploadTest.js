4c459ca8aa473a6265dacda5fbbec32211ad4d02
AriPenguin <achai@albany.edu>
2019-04-18 17:43:36 -0400

    started csv upload

--- testing/src/components/test/UploadTest.js
+++ testing/src/components/test/UploadTest.js
@@ -13,22 +13,23 @@ class UploadTest extends React.Component {
     }
 
     //take in csv file from rendering
-    onSubmit(e){
+    onSubmit(e) {
         e.preventDefault()
         //new test query with default setting (test title, test description, ...
         const newTest = {
             name: "TEST NAME",
             //null values: Inassignment description, start_time, end_time, ... is_multiple (boolean)
         }
-    //open csv file
+        //open csv file
+        //go through csv line by line (temp[6] for T/f, [10|12|14] for MC)
 
-    //go through csv line by line (temp[6] for T/f, [10|12|14] for MC)
+
         //line either: Q.num, Q.text, Ans1, Ans1.text, Ans2, Ans2.text, Ans3, Ans3.text, Ans4, Ans4.text, Ans5, Ans5.text, Ans6, Ans6.text
         //true & false: Q.num, Q.text, Ans1, Ans1.text, Ans2, Ans2.text
 
         //code in practice.js
-
-    render() {
+    }
+    render(){
         return (
         <div className="UploadTest">
             <div className="row">
