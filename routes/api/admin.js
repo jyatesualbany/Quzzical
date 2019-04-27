ca7a1826ae2970a08027176433d1420bfcb7e642
sean rowland <seanrowl@buffalo.edu>
2019-04-27 16:42:54 -0400

    added functionality to delete question from the db

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -325,6 +325,17 @@ router.get('/test', (req, result) => {
     
  })
 
+ router.post('/deleteQuestion', (req, result) => {
+   const question = req.body.question.qID
+   console.log(question);
+    db.query("delete from QUESTION where QUESTION_ID = '"+question+"'", (err, res) => {
+      if(err) throw err
+      console.log('it works');
+      return result.json({output: 'good'})
+    }) 
+
+ })
+
  router.post('/createTest', (req, result) => {
 
  })
--- testing/src/components/auth/AdminDashboard.js
+++ testing/src/components/auth/AdminDashboard.js
@@ -149,8 +149,11 @@ class Dashboard extends React.Component {
   deleteQuestion(question){
     // REMOVE TEST FROM DATABASE
     const tempList = this.state.questionList
-    tempList.splice(tempList.indexOf(question), 1)
-    this.setState({questionList : tempList})
+    axios.post('/api/admin/deleteQuestion', {question: question}) 
+      .then(res => {
+        tempList.splice(tempList.indexOf(question), 1)
+        this.setState({questionList : tempList})
+      })
   }
   createAdminTestTable = () => {
     let list = []
