3dd31f4a4bc115228419e6e82c8ba2447063983a
Cory <cmarriott@albany.edu>
2019-05-01 15:56:42 -0400

    adding ability to assign users to tests

--- testing/src/components/adminTest/AssignTest.js
+++ testing/src/components/adminTest/AssignTest.js
@@ -9,147 +9,148 @@ class AssignTest extends React.Component {
     super()
     this.state = {
       isAdmin : true,
-      testName: '',
-      testDesc: '',
-      testTime: '',
+      testName: props.location.state.test.tn,
+      testDesc: props.location.state.test.td,
+      testTime: props.location.state.test.tl,
       dateTime: null,
-      questionList : []
+      userList : []
     }
     this.onChange = this.onChange.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
-    this.createTest = this.createTest.bind(this)
+    this.assignTest = this.assignTest.bind(this)
     this.isChecked = this.isChecked.bind(this)
 
   }
-   
   componentDidMount(){
-    // axios.post('/api/admin/getQuestion', {})
-    //   .then(res => {
-    //     //console.log(res.data.ques)
-    //     var array = []
-    //     res.data.ques.forEach(x => {
-    //       if(x.IS_MULTIPLE == 0){
-    //         const quest = {
-    //           q: x.QUESTION_TEXT,
-    //           an1: x.ANSWER_ONE_TEXT,
-    //           an2: x.ANSWER_TWO_TEXT,
-    //           qID: x.QUESTION_ID,
-    //           correct: x.CORRECT,
-    //           isChecked: false
-    //         }
-    //         // console.log('this is the quest id' + quest.qID);
-    //         array.push(quest)
-    //         // console.log(array);
-            
-    //       }else{
-    //         var input = {}
-    //         input = this.makeObj(input, x)
-    //         // console.log(input);
-    //         array.push(input)
-    //       }
-    //     })
-    // console.log('state quest id: ' + this.state.questionList[0].qID)
-    //   })
-    
     axios.post('/api/admin/getUsers', {})
     .then(res => {
-        console.log(res.data.users)
+        //console.log(res.data.users)
         var array = []
         res.data.users.forEach(x => {
-            array.push(x.NAME)
+            array.push(x)
         })
-        this.setState({
-            questionList: array 
-        })
+        this.setState({ userList: array })
     })
   }
   onChange(e){
-    if(e.target.name == 'Test Description'){
-      this.setState({testDesc : e.target.value})
-    }else if(e.target.name == 'TestName'){
-      this.setState({testName: e.target.value})
-    }else if(e.target.name == 'Test Date'){
-      this.setState({dateTime: e.target.value})
-    }else{
-      this.setState({testTime: e.target.value})
-    }
   }
   onSubmit(){
     // e.preventDefault()
-    this.createTest()
+    this.assignTest()
   }
   isChecked(index){
-    console.log("questionList:", this.state.questionList)
-    let temp = this.state.questionList
+    console.log("TestName:", this.state.testName)
+    let temp = this.state.userList
     if(temp[index].isChecked === true){
       temp[index].isChecked = false
-      this.setState({ questionList : temp})
+      this.setState({ userList : temp})
     }else{
       temp[index].isChecked = true
-      this.setState({ questionList : temp})
+      this.setState({ userList : temp})
     }
   }
-  createQuestionTable = () => {
+  createUserTable = () => {
     let list = []
     list.push(
       <thead>
         <tr>
-          {/* <th scope="col">#</th> */}
+          <th scope="col">#</th>
           <th scope="col">Select User</th>
+          <th scope="col">User ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
           {/* <th scope="col">Delete? </th> */}
         </tr>
       </thead>
     )
     // console.log('length of list: ' + this.state.questionList.questionList[0].questionID);
     
-    for (let i = 0; i < this.state.questionList.length; i++) {
+    for (let i = 0; i < this.state.userList.length; i++) {
       let children = []
       children.push(<td className="align-middle">{i+1}</td>)
       children.push(<td className="align-middle">
         <input className="form-check-input text-align:center" type="checkbox" id="inlineCheckbox1" 
         onClick={this.isChecked.bind(this, i)}/>
       </td>)
-      children.push(<td className="align-middle">{this.state.questionList[i]}</td>)
+      children.push(<td className="align-middle">{this.state.userList[i].USER_ID}</td>)
+      children.push(<td className="align-middle">{this.state.userList[i].NAME}</td>)
+      children.push(<td className="align-middle">{this.state.userList[i].EMAIL}</td>)
       //Create the parent and add the children
       list.push(<tr>{children}</tr>)
     }
     return list
   }
-  createTest(){
-    let selectedQuestions = []
-    for(let i=0; i<this.state.questionList.length; i++){
-      if(this.state.questionList[i].isChecked ===  true){
-        selectedQuestions.push(this.state.questionList[i])
+  assignTest(){
+    let selectedUsers = []
+    for(let i=0; i<this.state.userList.length; i++){
+      if(this.state.userList[i].isChecked ===  true){
+        selectedUsers.push(this.state.userList[i])
       }
     }
+    //console.log("selected users:", selectedUsers)
+    //console.log('this the the create test fn: ' + this.state.testName);
     
     const input = {
-      users: selectedQuestions,
+      test: selectedUsers,
+      tName: this.state.testName,
+      tDes: this.state.testDesc,
+      tTime: this.state.testTime,
+      dateTime: this.state.dateTime
     } 
     
-    axios.post('/api/admin/assginTesr', input)
+    axios.post('/api/admin/createTest', input)
       .then(res => {
         console.log('it worked')
       })
+    //console.log("SELECTED QUESTIONS:", selectedUsers)
+    //console.log("Test Name:", this.state.testName)
+    // QUERY DB WITH LIST OF SELECTED QUESTIONS AND TEXT FROM INPUT BOXES
   }
   render() {
           return(
             // <form onChange={this.onChange}>
             <form onSubmit={this.onSubmit}>
+
             <div className="dashboard">
               <div className="row">
                 <div className="col-md-8 m-auto">
-                    <h1 className="display-4 text-center">Assgin TEST</h1>
+                    <h1 className="display-4 text-center">ASSIGN USERS TO TEST</h1>
                 </div>
               </div>
               <div className="row">
                 <div className="col-md-12">
                   <table className="table">
                     <tr>
+                      <th scope="col">Test Name:</th>
+                      <th scope="col">Test Description:</th>
+                      <th scope="col">Time Allotted (Minutes):</th>
+                      <th scope="col">Date of Test(YYYY-MM-DD HH:MI:SS):</th>
+                      <th scope="col">Create Test: </th>
+                    </tr>
+                    <tr>
+                      <td className="align-middle">
+                        <div className="form-group">
+                            <p>{this.state.testName}</p>
+                        </div>
+                      </td>
+                      <td className="align-middle">
+                        <div className="form-group">
+                            <p>{this.state.testDesc}</p>
+                        </div>
+                      </td>
+                      <td className="align-middle">
+                        <div className="form-group">
+                        <p>{this.state.testTime}</p>
+                        </div>
+                      </td>
+                      <td className="align-middle">
+                        <p>{this.state.testDate}</p>
+                      </td>
                       <td className="align-middle">
                         <div className="form-group">
                         <Link className="btn btn-success btn-space" to="/admindashboard"
-                            onClick={this.onSubmit.bind(this, this.state.questionList)}>Assgin Test</Link>
+                            // onClick={this.createTest.bind(this, this.state.questionList)}>Create Test</Link>
+                            onClick={this.onSubmit.bind(this, this.state.userList)}>Assign Users</Link>
                         </div>
                       </td>
                     </tr>
@@ -157,9 +158,9 @@ class AssignTest extends React.Component {
                 </div>
                 </div>
                 <div className="row">
-                    <h1 className="display-12 text-center m-auto">User Bank:</h1>
+                    <h1 className="display-12 text-center m-auto">Users:</h1>
                     <table className="table table-striped">
-                      {this.createQuestionTable()}
+                      {this.createUserTable()}
                     </table> 
               </div>
           </div>
